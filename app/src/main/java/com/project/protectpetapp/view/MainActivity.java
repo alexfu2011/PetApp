d695ac2bcce6d5e11c461f90e9f26fe7ac64c7e5
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-11 19:37:00 +0900

    2021-11-11 BottomNavigationView 설정

--- app/src/main/java/com/project/protectpetapp/view/MainActivity.java
+++ app/src/main/java/com/project/protectpetapp/view/MainActivity.java
@@ -2,6 +2,8 @@ package com.project.protectpetapp.view;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+
 import com.project.protectpetapp.R;
 import com.project.protectpetapp.databinding.ActivityMainBinding;
 
@@ -15,6 +17,19 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     protected void initView(Bundle savedInstanceState) {
         initToolbar();
         setToolbar("메인");
+
+        Fragment fragment_home = new HomeFragment();
+
+        mBinder.bottomNaviBar.setSelectedItemId(R.id.action_home);
+
+        mBinder.bottomNaviBar.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.action_home :
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment_home).commit();
+                    return true;
+            }
+            return false;
+        });
 
     }
 }
