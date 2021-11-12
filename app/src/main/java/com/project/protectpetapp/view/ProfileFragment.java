63144dce9b39ee204e2e4f83dd373e9784aa7119
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-13 00:42:44 +0900

    2021-11-13 ProfileFragment 추가

---
+++ app/src/main/java/com/project/protectpetapp/view/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.project.protectpetapp.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.project.protectpetapp.R;
+
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
