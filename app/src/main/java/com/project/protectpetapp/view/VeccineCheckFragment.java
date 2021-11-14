8a7acea6425eb768ec17b8c2c3e444534f750fc9
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-14 21:45:12 +0900

    2021-11-14 VeccineCheckFragment 추가 및 하단바 연결

---
+++ app/src/main/java/com/project/protectpetapp/view/VeccineCheckFragment.java
@@ -0,0 +1,18 @@
+package com.project.protectpetapp.view;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.project.protectpetapp.R;
+
+public class VeccineCheckFragment extends BaseFragment {
+
+    public VeccineCheckFragment() {
+        super(R.layout.fragment_veccine_check);
+    }
+
+    @Override
+    protected View initView(Bundle savedInstanceState) {
+        return null;
+    }
+}
