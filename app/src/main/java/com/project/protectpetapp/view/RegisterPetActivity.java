f7649b52ea2b4b7dc2fbe45b1203483e9f925b8f
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-16 00:06:37 +0900

    2021-11-16 RegisterPetActivity UI 수정

--- app/src/main/java/com/project/protectpetapp/view/RegisterPetActivity.java
+++ app/src/main/java/com/project/protectpetapp/view/RegisterPetActivity.java
@@ -1,10 +1,21 @@
 package com.project.protectpetapp.view;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+
 import com.project.protectpetapp.R;
 import com.project.protectpetapp.databinding.ActivityRegisterPetBinding;
 
-public class RegisterPetActivity extends BaseActivity<ActivityRegisterPetBinding> {
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class RegisterPetActivity extends BaseActivity<ActivityRegisterPetBinding> implements View.OnClickListener {
+
+    private final SimpleDateFormat mDateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
+    private long mDate;
 
 
     public RegisterPetActivity() {
@@ -15,10 +26,19 @@ public class RegisterPetActivity extends BaseActivity<ActivityRegisterPetBinding
     protected void initView(Bundle savedInstanceState) {
         initToolbar();
         String mode = getIntent().getStringExtra("mode");
-        if (mode.equals("dog")){
+        if (mode.equals("dog")) {
             setToolbar("강아지 등록");
-        }else if(mode.equals("cat")){
+        } else if (mode.equals("cat")) {
             setToolbar("고양이 등록");
+        }
+
+        mBinder.tvPetBirth.setOnClickListener(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_pet_birth) {
         }
     }
 }
