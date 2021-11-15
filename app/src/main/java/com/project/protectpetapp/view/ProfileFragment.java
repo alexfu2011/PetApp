39978b675a3462d374311ae4fa5df609451b815b
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-15 22:08:50 +0900

    2021-11-15 ProfileFragment, itemPetSelectDialog UI 구현, dialog 연결

--- app/src/main/java/com/project/protectpetapp/view/ProfileFragment.java
+++ app/src/main/java/com/project/protectpetapp/view/ProfileFragment.java
@@ -1,10 +1,8 @@
 package com.project.protectpetapp.view;
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.project.protectpetapp.R;
 import com.project.protectpetapp.databinding.FragmentProfileBinding;
@@ -18,35 +16,44 @@ public class ProfileFragment extends BaseFragment<FragmentProfileBinding> implem
 
     @Override
     protected View initView(Bundle savedInstanceState) {
-        fBinder.btnRegisterPet.setOnClickListener(this);
-        return fBinder.getRoot();
+        //fBinder.btnRegisterPet.setOnClickListener(this::onClick);
+        mBinder.btnRegisterPet.setOnClickListener(this);
+        return mBinder.getRoot();
     }
 
     @Override
     public void onClick(View v) {
-        View view = getLayoutInflater().inflate(R.layout.item_pet_select_dialog, null);
+        if (v.getId() == R.id.btn_register_pet) {
+            View view = getLayoutInflater().inflate(R.layout.item_pet_select_dialog, null);
 
-        TextView btnSelectDog = view.findViewById(R.id.btn_select_dog);
-        TextView btnSelectCat = view.findViewById(R.id.btn_select_cat);
+            //TextView btnSelectDog = view.findViewById(R.id.btn_select_dog);
+            //TextView btnSelectCat = view.findViewById(R.id.btn_select_cat);
 
-        //수정 필요 - 다이얼로그 안뜨는 오류
-        AlertDialog.Builder petSelectDialog = new AlertDialog.Builder(getActivity()).setView(view);
-        AlertDialog dialog = petSelectDialog.create();
-        dialog.show();
+            //수정 필요 - 다이얼로그 안뜨는 오류
+            AlertDialog.Builder petSelectDialog = new AlertDialog.Builder(getActivity());
+            petSelectDialog.setView(view)
+                    .create()
+                    .show();
 
-        String dog = "dog";
-        String cat = "cat";
+            String dog = "dog";
+            String cat = "cat";
 
-        btnSelectDog.setOnClickListener(v1 -> {
-            Intent intent = new Intent(getActivity(), RegisterPetActivity.class);
-            intent.putExtra("mode", dog);
-            startActivity(intent);
-        });
+/*
+            btnSelectDog.setOnClickListener(v1 -> {
+                Intent intent = new Intent(getActivity(), RegisterPetActivity.class);
+                intent.putExtra("mode", dog);
+                startActivity(intent);
+            });
 
-        btnSelectCat.setOnClickListener(v1 -> {
-            Intent intent = new Intent(getActivity(), RegisterPetActivity.class);
-            intent.putExtra("mode", cat);
-            startActivity(intent);
-        });
+            btnSelectCat.setOnClickListener(v1 -> {
+                Intent intent = new Intent(getActivity(), RegisterPetActivity.class);
+                intent.putExtra("mode", cat);
+                startActivity(intent);
+            });
+
+
+ */
+
+        }
     }
 }
