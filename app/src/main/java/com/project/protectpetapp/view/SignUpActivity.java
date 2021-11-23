0570bba44fa635382ff39fe93240697696f7ae71
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-23 23:25:31 +0900

    2021-11-23 회원가입 UI 수정

--- app/src/main/java/com/project/protectpetapp/view/SignUpActivity.java
+++ app/src/main/java/com/project/protectpetapp/view/SignUpActivity.java
@@ -8,26 +8,14 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.project.protectpetapp.R;
 import com.project.protectpetapp.databinding.ActivitySignUpBinding;
-import com.project.protectpetapp.model.Owner;
-
-import java.util.HashMap;
 
 
-public class SignUpActivity extends BaseActivity<ActivitySignUpBinding> {
-    FirebaseAuth firebaseAuth;
+public class SignUpActivity extends BaseActivity<ActivitySignUpBinding> implements View.OnClickListener {
+    private FirebaseAuth firebaseAuth;
     String text_password, text_password_ck;
-    Owner.OwnerInfo owners = new Owner.OwnerInfo(); // 주인
 
     public SignUpActivity() {
         super(R.layout.activity_sign_up);
@@ -39,29 +27,36 @@ public class SignUpActivity extends BaseActivity<ActivitySignUpBinding> {
         initToolbar();
         setToolbar("회원가입");
 
+        passwordTextChangedCheck();
+
         //파이어베이스 접근 설정
         firebaseAuth = FirebaseAuth.getInstance();
-        mBinder.pwState.setVisibility(View.INVISIBLE);
+        mBinder.txtSignPwState.setVisibility(View.INVISIBLE);
+        mBinder.btnSignUp.setOnClickListener(this);
+        mBinder.btnPhoneAuthStart.setOnClickListener(this);
 
-//        비밀번호 체크
-        mBinder.signupPwck.addTextChangedListener(new TextWatcher() {
+    }
+
+    private void passwordTextChangedCheck() {
+        //비밀번호 체크
+        mBinder.txtSignPasswordCheck.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                text_password = mBinder.signupPw.getText().toString();
-                text_password_ck = mBinder.signupPwck.getText().toString();
+                text_password = mBinder.txtSignPassword.getText().toString();
+                text_password_ck = mBinder.txtSignPasswordCheck.getText().toString();
 
                 if (text_password_ck.length() == 0 || text_password.length() == 0)
-                    mBinder.pwState.setVisibility(View.INVISIBLE);
+                    mBinder.txtSignPwState.setVisibility(View.INVISIBLE);
 
                 if (text_password.equals(text_password_ck)) {
-                    mBinder.pwState.setText("비밀번호가 일치합니다");
-                    mBinder.pwState.setVisibility(View.VISIBLE);
+                    mBinder.txtSignPwState.setText("비밀번호가 일치합니다");
+                    mBinder.txtSignPwState.setVisibility(View.VISIBLE);
                 } else {
-                    mBinder.pwState.setText("비밀번호가 일치하지 않습니다.");
+                    mBinder.txtSignPwState.setText("비밀번호가 일치하지 않습니다.");
                 }
             }
 
@@ -69,74 +64,73 @@ public class SignUpActivity extends BaseActivity<ActivitySignUpBinding> {
             public void afterTextChanged(Editable editable) {
             }
         });
+    }
 
-        //회원가입
-        mBinder.signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.btn_sign_up) {
+            signUp();
+        }else if (viewId == R.id.btn_phone_auth_start){
+            phoneAuthNumber();
+        }
+    }
 
-                //입력 정보 가져오기
-                final String email = mBinder.signupEmail.getText().toString();
-                final String password = mBinder.signupPw.getText().toString();
-                String pwcheck = mBinder.signupPwck.getText().toString();
+    private void signUp() {
+        //입력 정보 가져오기
+        String email = mBinder.txtSignEmail.getText().toString();
+        String password = mBinder.txtSignPassword.getText().toString();
+        String passwordCheck = mBinder.txtSignPasswordCheck.getText().toString();
+        String uid = firebaseAuth.getCurrentUser().getUid();
+        String name = mBinder.txtSignName.getText().toString().trim();
+        String phone;
 
-                if (password.equals(pwcheck)) {
-                    final ProgressDialog mDialog = new ProgressDialog(SignUpActivity.this);
-                    mDialog.setMessage("가입중입니다...");
-                    mDialog.show();
-
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-
-                            //가입 성공시
-                            if (task.isSuccessful()) {
-                                mDialog.dismiss();
-
-                                FirebaseUser user = firebaseAuth.getCurrentUser();
-                                String email = user.getEmail();
-                                String uid = user.getUid();
-                                String name = mBinder.signupName.getText().toString().trim();
-                                String pw = mBinder.signupPw.getText().toString().trim();
+        if (!password.equals(passwordCheck)) {
+            //비밀번호 오류시
+            Toast.makeText(SignUpActivity.this, "비밀번호가 틀렸습니다.\n 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
+        }
 
 
-                                //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
-                                HashMap<Object, String> hashMap = new HashMap<>();
-
-                                hashMap.put("uid", uid);
-                                hashMap.put("email", email);
-                                hashMap.put("name", name);
-                                hashMap.put("password", pw);
-
-                                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                                DatabaseReference reference = database.getReference("Users").child(uid).child("Userinfo");
-                                reference.setValue(hashMap);
+        final ProgressDialog mDialog = new ProgressDialog(SignUpActivity.this);
+        mDialog.setMessage("가입중입니다...");
+        mDialog.show();
 
 
-                                //가입이 이루어져을시 가입 화면을 빠져나감.
-                                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                                finish();
-                                Toast.makeText(SignUpActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignUpActivity.this, task -> {
+                    if (!task.isSuccessful()) {
+                        mDialog.dismiss();
+                        Toast.makeText(SignUpActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                    }
 
-                            } else {
-                                mDialog.dismiss();
-                                Toast.makeText(SignUpActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
-                                return;  //해당 메소드 진행을 멈추고 빠져나감.
+                    mDialog.dismiss();
+//                    Owner owner = Owner.builder()
+//                            .oid(uid)
+//                            .email(email)
+//                            .name(name)
+//                            .password(password)
+//                            .phone(phone)
+//                            .agree()
+//                            .build();
 
-                            }
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    Toast.makeText(SignUpActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
 
-                        }
-                    });
+                });
 
-                    //비밀번호 오류시
-                } else {
-                    Toast.makeText(SignUpActivity.this, "비밀번호가 틀렸습니다.\n 다시 입력해 주세요.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-        });
+    }
+
+    private void phoneAuthNumber(){
+//        PhoneAuthOptions options =
+//                PhoneAuthOptions.newBuilder(mAuth)
+//                        .setPhoneNumber(phone)       // Phone number to verify
+//                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+//                        .setActivity(this)                 // Activity (for callback binding)
+//                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+//                        .build();
+//        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 }
 
