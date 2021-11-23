f48d752954fe9f540ede3ccd28b582503daa7912
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-11-23 23:25:12 +0900

    2021-11-23 BuilderPattern 생성

--- app/src/main/java/com/project/protectpetapp/model/Owner.java
+++ app/src/main/java/com/project/protectpetapp/model/Owner.java
@@ -1,23 +1,24 @@
 package com.project.protectpetapp.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.Serializable;
 
-public class Owner {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner implements Serializable {
     public String oid;
     public String petId;
     public String name;
     public String phone;
     public String email;
     public String password;
-    public Map<String, OwnerInfo> Owners = new HashMap<>();
-
-    public static class OwnerInfo {
-        public String oid;
-        public String petId;
-        public String name;
-        public String phone;
-        public String email;
-        public String password;
-    }
+    public Boolean agree;
 }
