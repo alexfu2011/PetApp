c035e22d05e6e872a1a80a9041e4824a3e0db3b0
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-12-06 22:17:16 +0900

    2021-12-06 예방접종체크 UI 수정

--- app/src/main/java/com/project/protectpetapp/view/fragment/VeccineCheckFragment.java
+++ app/src/main/java/com/project/protectpetapp/view/fragment/VeccineCheckFragment.java
@@ -1,10 +1,12 @@
 package com.project.protectpetapp.view.fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.project.protectpetapp.R;
+import com.project.protectpetapp.databinding.FragmentVeccineCheckBinding;
 
-public class VeccineCheckFragment extends BaseFragment {
+public class VeccineCheckFragment extends BaseFragment<FragmentVeccineCheckBinding> implements View.OnClickListener {
 
     public VeccineCheckFragment() {
         super(R.layout.fragment_veccine_check);
@@ -12,5 +14,25 @@ public class VeccineCheckFragment extends BaseFragment {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        //todo : 반려동물 등록 여부 체크 후 등록 시 예방접종 체크리스트 확인 가능, 미등록시 등록 화면으로 이동
+        mBinder.layoutBinderPets.setOnClickListener(this);
+        mBinder.layoutTransparent.setOnClickListener(this);
+    }
+
+    public void petRegisterCheck(){
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.layout_binder_pets){
+            mBinder.layoutTransparent.setVisibility(View.VISIBLE);
+            mBinder.layoutPetSelect.setVisibility(View.VISIBLE);
+        }else if (viewId == R.id.layout_transparent){
+            mBinder.layoutTransparent.setVisibility(View.GONE);
+            mBinder.layoutPetSelect.setVisibility(View.GONE);
+        }
+
     }
 }
