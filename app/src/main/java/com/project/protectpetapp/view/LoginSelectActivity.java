6abc11aadcb0747abb30110bc3ad69c8c61c8b6b
natuure <simjayeon98@gmail.com>
2021-12-30 21:58:39 +0900

    2021-12-30 로그인 선택 UI 구현

---
+++ app/src/main/java/com/project/protectpetapp/view/LoginSelectActivity.java
@@ -0,0 +1,18 @@
+package com.project.protectpetapp.view;
+
+import android.os.Bundle;
+
+import com.project.protectpetapp.R;
+import com.project.protectpetapp.databinding.ActivityLoginSelectBinding;
+
+public class LoginSelectActivity extends BaseActivity<ActivityLoginSelectBinding> {
+
+    public LoginSelectActivity() {
+        super(R.layout.activity_login_select);
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+}
